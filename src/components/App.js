fad2217169a151f16a7165031f1c1fbf240f3afb
v1ct0rrx <velaz.victor@gmail.com>
2022-03-18 00:38:55 +0000

    Archivos pendientes

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,21 @@ import '../css/index.css';
 
 class App extends Component {
 
-  state = {
-    contacts: []
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+      contacts: []
+    }
+  }
+
+  handleItemDeleted(value) {
+    const index = this.state.contacts.indexOf(value, 0);
+    this.state.contacts.splice(index, 1);
+    this.setState({
+      contacts: this.state.contacts
+    });
   }
 
   componentDidMount = () => {
@@ -31,47 +44,53 @@ class App extends Component {
       dataIndex: 'id',
       key: 'id',
       width: 100,
+      className: 'columna'
     },
     {
       title: 'Nombre',
       dataIndex: 'name',
       key: 'name',
       width: 100,
+      className: 'columna'
     },
     {
       title: 'Usuario',
       dataIndex: 'username',
       key: 'username',
       width: 200,
+      className: 'columna'
     },
     {
       title: 'Correo',
       dataIndex: 'email',
       key: 'email',
       width: 200,
+      className: 'columna'
     },
     {
       title: 'Editar',
       dataIndex: '',
       key: 'operations',
+      className: 'columna',
       render: (value) => <Boton texto={'Editar'} handleClick={() => this.handleClickEditar(value)} />,
     },
     {
       title: 'Eliminar',
       dataIndex: '',
       key: 'operations',
+      className: 'columna',
       render: (value) => <Boton texto={'Eliminar'} handleClick={() => this.handleClickEliminar(value)} />,
     }
   ];
 
   handleClickEditar = (value) => {
-    alert('Editar');
+    alert('Editando :: ' + value.id);
   };
 
   handleClickEliminar = (value) => {
-    alert('Eliminar');
-    const index = this.state.contacts.indexOf(value, 0);
-    this.state.contacts.splice(index, 1);
+    alert('Eliminando :: ' + value.id);
+    /*const index = this.state.contacts.indexOf(value, 0);
+    this.state.contacts.splice(index, 1);*/
   }
 
 }
